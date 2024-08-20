673ab0b35f488936ab3d57c355689a2b21993767
Samuel Aberenika <samuelaberenika@proton.me>
2024-08-20 14:21:50 +0100

    Add git ignore

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.print("MANEEEE IM HELLA CONFUSED");
-        System.out.println("I WANNA BE AWESOME, Version3.0");
+        System.out.println("I WANNA BE AWESOME, Version4.0");
     }
 }
